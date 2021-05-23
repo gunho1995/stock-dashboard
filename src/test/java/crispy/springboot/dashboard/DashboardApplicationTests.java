60e9f7d86e0f03b6ab4fd60e29ee49fb74cb95d0
Crispy <gunho19945@gmail.com>
2021-05-23 16:23:04 +0900

    개발환경 세팅

---
+++ src/test/java/crispy/springboot/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package crispy.springboot.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
